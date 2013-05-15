8499fdf16deed4519e6c317b9948dd3f42517aa9
Laura Steadman <lauramsteadman@gmail.com>
2013-05-15 14:47:33 -0600

    jquery sliding of photos up and down setup

---
+++ app/assets/javascripts/feed.js
@@ -0,0 +1,20 @@
+//This loads the first image to show on the FEEDS SHOW page
+$(document).ready(function(){
+  $('.show-photo').first().toggleClass('hidden');
+
+  $(".slideup").click(function(){
+      var photo = $(this).parent();
+      $(photo).next().toggleClass('hidden');
+      $(photo).slideUp(500, function(){
+          $(photo).toggleClass('hidden');
+      });
+   });
+
+  $(".slidedown").click(function(){
+      var photo = $(this).parent();
+      $(photo).prev().toggleClass('hidden');
+      $(photo).prev().slideDown(500, function(){
+        $(photo).toggleClass('hidden');
+      });
+   });
+});
