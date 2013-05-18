642795137690c0a799882e47a2e96c94d28c7b70
Jennifer Eliuk <jen7maples@gmail.com>
2013-05-18 11:51:08 -0600

    Create sessions table, use ActiveRecord store instead of Cookie Store. Creating a user is working, may need to remove code in User model we probably aren't using any more. Email tests still failing, emails are now not going through.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130515142814) do
+ActiveRecord::Schema.define(:version => 20130518174518) do
 
   create_table "feed_items", :force => true do |t|
     t.string  "photo_url"
@@ -41,6 +41,16 @@ ActiveRecord::Schema.define(:version => 20130515142814) do
   end
 
   add_index "searches", ["feed_id"], :name => "index_searches_on_feed_id"
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "users", :force => true do |t|
     t.string   "provider"
