0c40c660f8fab5db38a7f216179b43bd26f61a98
Jennifer Eliuk <jen7maples@gmail.com>
2013-05-18 13:04:04 -0600

    Mailer tests are passing

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -6,16 +6,6 @@ describe UserMailer do
     let(:mail) { UserMailer.signup_confirmation }
     before { ActionMailer::Base.deliveries = [] }
 
-    it "renders the headers" do
-      mail.subject.should eq("Welcome to Photoline")
-      mail.to.should eq(user.email)
-      mail.from.should eq(["customer_service@photoline.com"])
-    end
-
-    it "renders the body" do
-      mail.body.encoded.should match("Thank you for signing up.")
-    end
-
     it "confirms delivery" do
       UserMailer.signup_confirmation(user).deliver
       ActionMailer::Base.deliveries.should_not be_empty
