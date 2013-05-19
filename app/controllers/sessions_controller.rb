96818961a9ce38b58489da66f7e181ef532ecf97
Danny Garcia <dannygarcia.me@gmail.com>
2013-05-18 23:27:31 -0600

    "When user is logged in root path will be dashboard_path (temporary), when user is logged in a menu button is visable from the nav bar and when clicked shows a menu bar that shifts the page to the right. Still needs lots of work"

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,6 +19,7 @@ class SessionsController < ApplicationController
   end
 
   def home
+    redirect_to dashboard_path if current_user
   end
 end
 
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
 FeedEngine::Application.config.session_store :active_record_store
+FeedEngine::Application.config.session_store :cookie_store, :key => '_tourlyapp_session', :domain => "lvh.me"
+
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
