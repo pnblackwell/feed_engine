128159e15c8c38d9084f7bfa553fb880d54227b0
Danny Garcia <dannygarcia.me@gmail.com>
2013-05-20 14:38:03 -0600

    500px

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,7 +2,7 @@ class Search < ActiveRecord::Base
   belongs_to :feed
   has_many :feed_items
 
-  attr_accessible :search_type, :value
+  attr_accessible :search_type, :value, :search_source
 
   def generate_feed_items
     # if self.source == 'flickr'
