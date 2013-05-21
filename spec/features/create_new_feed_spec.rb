4d36c973c21a1cee85028bdaa20ea00002d4937c
Danny Garcia <dannygarcia.me@gmail.com>
2013-05-21 15:03:41 -0600

    update create_new_feed_spec

--- spec/features/create_new_feed_spec.rb
+++ spec/features/create_new_feed_spec.rb
@@ -15,6 +15,7 @@ describe 'Create new feed' do
     fill_in "feed_name", with: "Jen's Pics"
     fill_in "feed_subdomain", with: "jenspics"
     fill_in "value", with: "white raven photography"
+    check "flickr"
     find(:css, "#feed_searches_attributes_0_search_type_username").set(true)
     click_link_or_button "Create Feed"
 
@@ -28,6 +29,7 @@ describe 'Create new feed' do
     fill_in "feed_name", with: "Paul's Pics"
     fill_in "feed_subdomain", with: "paulspics"
     fill_in "value", with: "horses"
+    check "flickr"
     find(:css, "#feed_searches_attributes_0_search_type_keyword").set(true)
     click_link_or_button "Create Feed"
 
