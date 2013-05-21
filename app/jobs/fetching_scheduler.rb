478d146f1a56478ce3f3ab3acaba6116ccd19c44
Laura Steadman <lauramsteadman@gmail.com>
2013-05-20 18:49:20 -0600

    they work hard for the money, in the background

--- app/jobs/fetching_scheduler.rb
+++ app/jobs/fetching_scheduler.rb
@@ -2,7 +2,6 @@ class FetchingScheduler
   @queue = :schedule
 
   def self.perform
-    resque.logger.info "Schedule all things for all the times"
     Search.all.each do |search|
       Resque.enqueue(PhotoFetcher, search.id)
     end
