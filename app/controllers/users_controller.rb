196309d676e0c000324c37f0677f9207eb514fe6
Danny Garcia <dannygarcia.me@gmail.com>
2013-05-21 21:43:13 -0600

    refactor app/controllers/users_controller.rb to create user with only the relevant user info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,7 @@ class UsersController < ApplicationController
 
   def create
     user_details_from_form = params[:user]
-    user_details_from_omniauth = omniauth_user_info(session[:omniauth_results])
-
-    @user = User.create(user_details_from_form.merge(user_details_from_omniauth))
+    @user = User.create(user_details_from_form.merge(session[:user_info]))
     if @user.save
       session[:user_id] = @user.id
       redirect_to dashboard_path
@@ -22,15 +20,5 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @feeds = @user.feeds
-  end
-
-private
-
-  def omniauth_user_info(omniauth_details)
-    username = omniauth_details['info']['nickname']
-    provider = omniauth_details['provider']
-    provider_id = omniauth_details['uid']
-
-    {username: username, provider: provider, provider_id: provider_id}
   end
 end
