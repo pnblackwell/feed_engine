08f230c4e565ed642747f7260ce7a59f86cfbb7c
Danny Garcia <dannygarcia.me@gmail.com>
2013-05-21 18:38:11 -0600

    fixed session store, dont touch it

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 unless Rails.env.test?
-  FeedEngine::Application.config.session_store :cookie_store, :key => '_tourlyapp_session', :domain => "lvh.me"
+FeedEngine::Application.config.session_store :active_record_store
+FeedEngine::Application.config.session_store :cookie_store, :key => '_tourlyapp_session', :domain => "lvh.me"
 end
 
 
-FeedEngine::Application.config.session_store :active_record_store
 
